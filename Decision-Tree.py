5ddf1c231c503423b3263e57e9533fbccd5618ff
HainZz <61663467+HainZz@users.noreply.github.com>
2022-04-06 15:44:05 -0400

    Creation of Basic Working Model with 0.993% AR

--- Decision-Tree.py
+++ Decision-Tree.py
@@ -1,12 +1,13 @@
 import pandas as pd 
 import numpy as np
-from pathlib import Path
 import matplotlib.pyplot as plt 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import plot_tree
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import plot_confusion_matrix
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+import pickle
 
 FEATURES_TO_SELECT = 15
 
@@ -126,15 +127,81 @@ def FeatureSelection(dataset): #Purpose of this function is to control the numbe
     print(list(dataset))
     return dataset
 
+#Sci-Kit natively does not support strings therefore its important to encode values 
+#SOURCE: https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.LabelEncoder.html
+#This converts Benign = 0, PortScan = 1
+
+def GetColumnIndex(columnName,df):
+    ColumnIndex = df.columns.get_loc(columnName)
+    #print(ColumnIndex)
+    return ColumnIndex
+
+def EncodeLabels(dataset):
+    le  = LabelEncoder()
+    column = "Label"
+    ColumnIndex = GetColumnIndex(column,dataset)
+    dataset.iloc[:,ColumnIndex] = le.fit_transform(dataset.iloc[:,ColumnIndex])
+    #print(dataset['Label'].unique()) #Testing that we have 0,1 as the new Label values.
+    return dataset  
+
 def DatasetImport():
     #Read In Dataset
-    dataset = pd.read_csv('Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv',header=None,dtype=object) #Note this file is placed within the same directory as the program if u want to run this either change this to match the file location of downloaded CSV
+    dataset = pd.read_csv('C:\Project\Code\Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv',header=None,dtype=object) #Note this file is placed within the same directory as the program if u want to run this either change this to match the file location of downloaded CSV
     dataset = ApplyColumnNames(dataset) #Apply's Column Names to all columns before removal. This helps format dataset easier 
+    print(dataset['Label'].value_counts())
     dataset = FeatureSelection(dataset) #This selects the features to be classified & Removes non features.
-
+    dataset = EncodeLabels(dataset)
+    dataset.to_csv('Code\Formatted_CSV\output.csv')
    
+## BEGGINING OF ACTUAL DECISION TREE CODE
+
+#SOURCE: https://machinelearningmastery.com/train-test-split-for-evaluating-machine-learning-algorithms/
+def GetSplitData(X,Y):
+    #This splits our entire dataset into 77% training data and 33% test data 
+    X_train,X_test,Y_train,Y_test = train_test_split(X,Y, test_size = 0.33)
+    return X_train,X_test,Y_train,Y_test
+
+def DecisionTree():
+    df = pd.read_csv('C:\Project\Code\Formatted_CSV\output.csv')
+    LabelIndex = GetColumnIndex("Label", df)
+    ##Separting Class labels from data. This is required for classification theres no point putting the lables within X training samples 
+    LabelTrainingSamples = df.iloc[:,LabelIndex]
+    NonLabelTrainingSamples = df.drop(df.columns[LabelIndex],axis=1)
+    X_train,X_test,Y_train,Y_test = GetSplitData(NonLabelTrainingSamples,LabelTrainingSamples)
+    CreateTreeModel(X_train,X_test,Y_train,Y_test)
+
+
+##SOURCE: https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
+def GenerateConfusionMatrix(X_test,Y_test,model):
+    titles_options = [("Unormalized Confusion Matrix",None),("Normalized Confusion Matrix","true")]
+    for title, normalize in titles_options:
+        disp = ConfusionMatrixDisplay.from_estimator(
+            model,
+            X_test,
+            Y_test,
+            display_labels=["PortScan","Benign"],
+            cmap=plt.cm.Blues,
+            normalize=normalize,
+        )
+        disp.ax_.set_title(title)
+        print(title)
+        print(disp.confusion_matrix)
+    plt.savefig()
+
+def CreateTreeModel(X_train,X_test,Y_train,Y_test):
+    model = DecisionTreeClassifier()
+    model.fit(X_train,Y_train)
+    print("Training Decision Tree Complete")
+    prediction = model.predict(X_test)
+    print(prediction)
+    pred_acc = accuracy_score(Y_test, prediction)
+    print(pred_acc)
+    GenerateConfusionMatrix(X_test,Y_test,model)
+    pickle.dump(model,open("C:\Project\Model_Dump\model.obj","wb"))
+
 def main():
     DatasetImport()
+    DecisionTree()
 
 
 main()
